e8e58d283c43aefbafda9ce1614b615b801aef7a
plamensh95 <plamensh95@gmail.com>
2019-09-04 20:46:48 +0300

    ASD/Feature 255: Finished with this test

--- AppDelegate.swift
+++ AppDelegate.swift
@@ -55,7 +55,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
     }
     
-    // MARK: - Core Data stack
+    // MARK: - Core Data stack containers
     
     lazy var persistentContainer: NSPersistentContainer = {
         /*
